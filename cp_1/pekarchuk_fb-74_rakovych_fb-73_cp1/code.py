ef75d8613a6c23330cab2cdb31240a910088c8f4
pinlast1 <pekarchukdanil@gmail.com>
2019-09-25 20:05:42 +0300

    add step

--- cp_1/pekarchuk_fb-74_rakovych_fb-73_cp1/code.py
+++ cp_1/pekarchuk_fb-74_rakovych_fb-73_cp1/code.py
@@ -19,11 +19,11 @@ def remove_not_alpha(data, need_space):
     return return_list
 
 
-def get_ngram_dict(lines, ngram_num=2):
+def get_ngram_dict(lines, ngram_num=2, step=2):
     ngram_counter = 0
     ngram_dict = defaultdict(int)
     for line in lines:
-        for i in range(0, len(line), ngram_num):
+        for i in range(0, len(line), step):
             ngram = line[i:i+ngram_num]
             if len(ngram) != ngram_num:
                 continue
@@ -62,10 +62,11 @@ def main():
     num = int(sys.argv[1]) if len(sys.argv) >= 2 else 2
     need_space = eval(sys.argv[2]) if len(sys.argv) >= 3 else False
     fname = sys.argv[3] if len(sys.argv) >= 4 else 'TEXT'
+    step = sys.argv[4] if len(sys.argv) >= 5 else num
 
     lines = get_file_lines(fname)
     filtered_lines = remove_not_alpha(lines, need_space)
-    ngram_dict = get_ngram_dict(filtered_lines, num)
+    ngram_dict = get_ngram_dict(filtered_lines, num, step=step)
 
     entropy_val = get_entropy(ngram_dict, num)
     redundancy_val = get_redundancy(entropy_val)
